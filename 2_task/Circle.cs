464992aa592b3dd62d9459271ed8b9cb17e7be61
KourtKardash <99260475+KourtKardash@users.noreply.github.com>
2024-04-14 17:48:25 +0300

    Add files via upload

---
+++ 2_task/Circle.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareLibrary
+{
+    public interface Shape
+    {
+        double GetSquare();
+    }
+    public class Circle : Shape
+    {
+        private double _radius;
+        public Circle(double radius) 
+        {
+            if (_radius <= 0)
+            {
+                throw new ArgumentException("Радиус круга должен быть строго больше ноля.");
+            }
+            _radius = radius;
+        }
+        public double GetSquare()
+        {
+            return Math.PI * _radius * _radius;
+        }
+    }
+    public class Triangle : Shape
+    {
+        private double _side1, _side2, _side3;
+        public  Triangle(double side1, double side2, double side3)
+        {
+            _side1 = side1;
+            _side2 = side2;
+            _side3 = side3;
+        }
+        public double GetSquare()
+        {
+            if (_side1 <= 0 || _side2 <= 0 || _side3 <= 0)
+            {
+                throw new ArgumentException("Стороны треугольника должны быть строго больше ноля.");
+            }
+            if(!(_side1 + _side2 > _side3 && _side2 + _side3 > _side1 && _side1 + _side3 > _side2))
+            {
+                throw new ArgumentException("Треугольника с такими сторонами не существует.");
+            }
+            double p = 0.5 * (_side1 + _side2 + _side3);
+            return Math.Sqrt(p * (p - _side1) * (p - _side2) * (p - _side3));
+        }
+        public bool IsRight()
+        {
+            double[] sides = { _side1, _side2, _side3 };
+            Array.Sort(sides);
+            if (sides[0] * sides[0] + sides[1] * sides[1] == sides[2] * sides[2])
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ 2_task/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //test1();
+                //test2();
+                //test3();
+                //test4();
+                //test5();
+                //test6();
+                test7();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+        static void test1()
+        {
+            Circle a = new Circle(2);
+            Console.WriteLine(a.GetSquare());
+            Triangle b = new Triangle(1, 3, 3);
+            Console.WriteLine(b.GetSquare());
+        }
+        static void test2()
+        {
+            Shape a = new Circle(2);
+            Console.WriteLine(a.GetSquare());
+            Shape b = new Triangle(1, 3, 3);
+            Console.WriteLine(b.GetSquare());
+        }
+        static void test3()
+        {
+            Circle a = new Circle(0);
+            Console.WriteLine(a.GetSquare());
+        }
+        static void test4()
+        {
+            Shape b = new Triangle(1, 2, 3);
+            Console.WriteLine(b.GetSquare());
+        }
+        static void test5()
+        {
+            Shape b = new Triangle(-2, 2, 2);
+            Console.WriteLine(b.GetSquare());
+        }
+        static void test6()
+        {
+            Triangle b = new Triangle(3, 4, 5);
+            Console.WriteLine(b.GetSquare());
+            Console.WriteLine(b.IsRight());
+        }
+        static void test7()
+        {
+            Triangle b = new Triangle(3, 4, 6);
+            Console.WriteLine(b.GetSquare());
+            Console.WriteLine(b.IsRight());
+        }
+    }
+}
